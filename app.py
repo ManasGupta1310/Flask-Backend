d53da5a14c6fdc142746da4aa29cb2d9ac849545
Manas Gupta <mmanasggupta@gmail.com>
2022-03-05 22:01:43 +0530

    Fix

--- app.py
+++ app.py
@@ -179,5 +179,61 @@ def orders():
 
     return jsonify(obj)
 
+@app.route("/products")
+@cross_origin()
+def products():
+  obj=[
+    {
+      "product":"Mc ALoo Tikki",
+      "category":"Burger",
+      "price":"50",
+      "inStock":True,
+    },
+    {
+      "product":"Mc Veggie",
+      "category":"Burger",
+      "price":"60",
+      "inStock":True,
+    },
+    {
+      "product":"Mc Flurry",
+      "category":"Ice Cream",
+      "price":"60",
+      "inStock":True,
+    },
+    {
+      "product":"Pepsi",
+      "category":"Drinks",
+      "price":"70",
+      "inStock":True,
+    }
+  ]
+  return jsonify(obj)
+
+@app.route("/categories")
+@cross_origin()
+def categories():
+  obj=[
+    {
+      "title": "Pizza",
+      "description": "A wide variety of mouth-watering pizzas in three categories : small, medium and large.",
+      "img": "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGl6emF8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      "status": True
+    },
+    {
+      "title": "Extras",
+      "description": "Breadsticks, dips, desserts and many more to compliment your delicious Pizza meal !!",
+      "img": "https://images.unsplash.com/photo-1469648034646-7911874fe62b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YXJsaWMlMjBicmVhZHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
+      "status": True
+    },
+    {
+      "title": "Beverages",
+      "description": "A wide range of hot and cold beverages to complete your perfect meal.",
+      "img": "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmV2ZXJhZ2VzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      "status": True
+    }
+  ]
+  return jsonify(obj)
+
 if __name__=="__main__":
     app.run(debug=True)
