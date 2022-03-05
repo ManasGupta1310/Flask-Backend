a70441d9bb63d8ac73d62402e47393083a6f6472
Manas Gupta <mmanasggupta@gmail.com>
2022-03-05 17:57:18 +0530

    fix

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify
-
+from flask_cors import cross_origin, CORS
 app = Flask(__name__)
+cors=CORS(app)
+
+app.config['CORS_HEADERS']='Content-Type'
 
 @app.route("/")
+@cross_origin()
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/stores")
+@cross_origin()
 def stores():
     obj=[
   {
@@ -124,6 +129,7 @@ def stores():
     return jsonify(obj)
 
 @app.route("/orders")
+@cross_origin()
 def orders():
     obj=[
   {
